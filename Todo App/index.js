901c9fd2fde3615620d3bf9e51815fb8781a591c
Jitendra Sengar <jitendrasengar1517@gmail.com>
2024-04-07 09:25:06 +0530

    commit weather app and calculator

---
+++ Todo App/index.js
@@ -0,0 +1,37 @@
+const inputBox=document.querySelector("input-box");
+const listContainer=document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value===''){
+        alert("You must write something!")
+        
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        saveData();
+    }
+    inputBox.value='';
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagname==='LI'){
+        e.target.classList.toggle("checked")
+        saveData();
+        
+    }
+    else if(e.target.tagname==='SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data")
+}
+
